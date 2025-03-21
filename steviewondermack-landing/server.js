7d35c72b855a3265e0ff1e2aa0f029d5ea5a27de
EC2 Default User <ec2-user@ip-172-31-15-202.us-east-2.compute.internal>
2025-03-21 19:27:08 +0000

    restructured folders removed auth folder and made landing parent folder

--- steviewondermack-landing/server.js
+++ steviewondermack-landing/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const authRoutes = require('./auth');
+
 const app = express();
 const port = 3000;
 
 // Serve static files from Steviewondermack-landing/public
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Use authentication routes
+app.use('/auth', authRoutes);
 
 // Serve the main landing page
 app.get('/', (req, res) => {
