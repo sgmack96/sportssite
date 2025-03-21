81e42d9b05d979333b10116c698f033e466d34ff
EC2 Default User <ec2-user@ip-172-31-15-202.us-east-2.compute.internal>
2025-03-21 19:06:15 +0000

    i dont really know

--- steviewondermack-auth/server.js
+++ steviewondermack-auth/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const authRoutes = require('./auth');
 
 const app = express();
+const port = 3000;
 
 // Serve static files from Steviewondermack-landing/public
 app.use('/landing', express.static(path.join(__dirname, '../Steviewondermack-landing/public')));
@@ -13,6 +14,11 @@ app.use('/auth', express.static(path.join(__dirname, 'public')));
 // Use authentication routes
 app.use('/auth', authRoutes);
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+// Serve the main landing page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Steviewondermack-landing/public/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
